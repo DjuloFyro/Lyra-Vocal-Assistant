f3cc2003d49e88fe0b2844ebb0fe71a797bfa618
Julian Gil <juliangil2424@gmail.com>
2023-07-03 15:29:35 +0200

    add: method think

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import vocal_assistant as va
 def main():
     vocal_assistant = va.VocalAssistant()
     audio_str = vocal_assistant.listen()
-    print(audio_str)
+    response = vocal_assistant.think(audio_str)
+    print(f"User: {audio_str}")
+    print(f"Vocal Assistant: {response}")
 
 
 if __name__ == "__main__":
--- vocal_assistant.py
+++ vocal_assistant.py
@@ -3,25 +3,27 @@ import speech_recognition as sr
 import os
 
 
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
-
 class VocalAssistant():
     def __init__(self):
-        return
+        self.OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
     def listen(self) -> str:
         r = sr.Recognizer()
         with sr.Microphone() as source:
             audio = r.listen(source)
         try:
-            audio_str = r.recognize_whisper_api(audio, api_key=OPENAI_API_KEY)
+            audio_str = r.recognize_whisper_api(audio, api_key=self.OPENAI_API_KEY)
         except sr.RequestError as e:
             print("Could not request results from Whisper API")
         return audio_str
 
+    def think(self, prompt: str) -> str:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "system", "content": prompt}],
+            temperature=0.6
+        )
+        return dict(response.choices[0])["message"]["content"]
+
     def speak(self) -> None:
         pass
-
-    def think(self) -> None:
-        pass
-
