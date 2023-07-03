dd131eb07235dc2bddfea26601fc1fd879650399
Julian Gil <juliangil2424@gmail.com>
2023-07-03 14:05:05 +0200

    add: project initialization

---
+++ vocal_assistant.py
@@ -0,0 +1,16 @@
+import openai
+
+class VocalAssistant():
+    def __init__(self) -> None:
+        pass
+
+
+    def listen(self) -> None:
+        pass
+
+    def speak(self) -> None:
+        pass
+
+    def think(self) -> None:
+        pass
+
