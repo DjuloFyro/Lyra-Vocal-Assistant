64490a1f387c0e50562afeb3d69d3f5eadcc42df
Julian Gil <juliangil2424@gmail.com>
2023-07-03 16:09:07 +0200

    add: v1 working

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import vocal_assistant as va
 
 def main():
     vocal_assistant = va.VocalAssistant()
-    audio_str = vocal_assistant.listen()
-    response = vocal_assistant.think(audio_str)
-    print(f"User: {audio_str}")
-    print(f"Vocal Assistant: {response}")
 
-    vocal_assistant.speak(response)
+    while True:
+        audio_str = vocal_assistant.listen()
+        response = vocal_assistant.think(audio_str)
+        print(f"User: {audio_str}")
+        print(f"Vocal Assistant: {response}")
+        vocal_assistant.speak(response)
 
 
 if __name__ == "__main__":
