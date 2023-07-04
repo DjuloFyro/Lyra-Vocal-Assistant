a4945b1de0539790c0348d5cc31ffa922cbf22bc
Julian Gil <juliangil2424@gmail.com>
2023-07-04 19:09:54 +0200

    add: comments and docstring

--- src/main.py
+++ src/main.py
@@ -1,20 +1,36 @@
 import vocal_assistant as va
 import speech_recognition as sr
 
-def chatting(vocal_assistant: va.VocalAssistant)-> None:
+def chatting(vocal_assistant: va.VocalAssistant) -> None:
+    """
+    Listens to the user's audio input, processes it, and responds.
+
+    Args:
+        vocal_assistant (va.VocalAssistant): Instance of the VocalAssistant class.
+    """
     audio_str = vocal_assistant.listen()
+
     if audio_str != "":
         response = vocal_assistant.think(audio_str)
         print(f"User: {audio_str}")
         print(f"Vocal Assistant: {response}")
         vocal_assistant.speak(response)
 
-def listen_for_keyword(keyword:str, vocal_assistant: va.VocalAssistant) -> None:
+def listen_for_keyword(keyword: str, vocal_assistant: va.VocalAssistant) -> None:
+    """
+    Listens for a specific keyword and triggers the chatting function.
+
+    Args:
+        keyword (str): Keyword to listen for.
+        vocal_assistant (va.VocalAssistant): Instance of the VocalAssistant class.
+    """
     r = sr.Recognizer()
+
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source, duration=3)
         print("Listening...")
         audio = r.listen(source, timeout=3, phrase_time_limit=30)
+
     try:
         # Use Google Speech Recognition to convert audio to text
         text = r.recognize_google(audio)
@@ -28,9 +44,10 @@ def listen_for_keyword(keyword:str, vocal_assistant: va.VocalAssistant) -> None:
     except sr.RequestError as e:
         print("Could not request results; {0}".format(e))
 
-
-
 def main():
+    """
+    Main function to execute the vocal assistant program.
+    """
     keyword = "ok google"
     vocal_assistant = va.VocalAssistant()
     
